e605a18e71c43b94f98570e61a1f5608a6b889a2
Alex <alexsteanly@mail.ru>
2023-06-18 21:27:26 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,76 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
-}
+func Unpack(s string) (string, error) {
+	var result strings.Builder
+	var prev rune
+	var count int
+	// fmt.Println(s)
+	lenString := len(s)
+
+	for index, r := range s {
+		
+		// Проверяем - является ли первый символ в строке цифрой, если да - возвращаем ошибку
+		if unicode.IsDigit(r) {
+			if prev == 0 {
+				return "", ErrInvalidString
+			} else {
+				// Если идут две цифры подряд - возвращам ошибку
+				if unicode.IsDigit(r) && unicode.IsDigit(prev) {
+					return "", ErrInvalidString
+				}
+				if string(prev) != `\` {
+					count, _ = strconv.Atoi(string(r))
+					// обрабатываем случай появления цифры 0 в строке
+					if count != 0 {
+						result.WriteString(strings.Repeat(string(prev), count))
+						prev = r
+						count = 0
+				}
+					prev = r
+				} else {
+					result.WriteString(string(r))
+					prev = r
+				}
+
+			}
+
+		} else {
+			// Записываем литералы в новую строку. Чтобы избежать появления нечитаемых символов в строке вводим условие с index > 0
+			// т.к. при index == 0 условие unicode.IsDigit(prev) == false будет верным, но prev в этом случае будет в состоянии ZeroValue
+			if unicode.IsDigit(prev) == false && index > 0 {
+				if string(r) != `\` {
+					result.WriteRune(prev)
+					prev = r
+				} else {
+					if string(prev) == `\` && unicode.IsDigit(r) {
+						result.WriteRune(r)
+						prev = r
+					} else {
+						if unicode.IsDigit(prev) && string(r) == `\`{
+							prev = r
+						} else {
+							result.WriteRune(prev)
+							prev = r
+						}	
+					}
+				}
+			} else {
+				prev = r
+			}
+			// Запись последней руны в строке
+			if lenString == index+1 {
+				result.WriteRune(r)
+			}
+
+		}
+	}
+	return result.String(), nil
+}	
