b566cdfee54c8f87625c6f3deea51bf3eb772a0f
Alex <alexsteanly@mail.ru>
2023-06-18 21:41:22 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,7 +13,6 @@ func Unpack(s string) (string, error) {
 	var result strings.Builder
 	var prev rune
 	var count int
-	// fmt.Println(s)
 	lenString := len(s)
 
 	for index, r := range s {
@@ -40,12 +39,8 @@ func Unpack(s string) (string, error) {
 					result.WriteString(string(r))
 					prev = r
 				}
-
 			}
-
 		} else {
-			// Записываем литералы в новую строку. Чтобы избежать появления нечитаемых символов в строке вводим условие с index > 0
-			// т.к. при index == 0 условие unicode.IsDigit(prev) == false будет верным, но prev в этом случае будет в состоянии ZeroValue
 			if unicode.IsDigit(prev) == false && index > 0 {
 				if string(r) != `\` {
 					result.WriteRune(prev)
@@ -66,11 +61,9 @@ func Unpack(s string) (string, error) {
 			} else {
 				prev = r
 			}
-			// Запись последней руны в строке
 			if lenString == index+1 {
 				result.WriteRune(r)
 			}
-
 		}
 	}
 	return result.String(), nil
