5d380f82e30a16ad6e826bd1f7670cc8d57a3f0d
Alex <alexsteanly@mail.ru>
2023-06-29 12:57:29 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,20 +11,25 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+const slash string = "slash"
+const digit string = "digit"
+const letter string = "letter"
+const stop string = "stop"
+
 func makeConf(confString string) []string {
 	var confSlice []string
 
 	for _, i := range confString {
 		switch {
 		case string(i) == `\`:
-			confSlice = append(confSlice, "slash")
+			confSlice = append(confSlice, slash)
 		case unicode.IsDigit(i):
-			confSlice = append(confSlice, "digit")
+			confSlice = append(confSlice, digit)
 		case unicode.IsLetter(i):
-			confSlice = append(confSlice, "letter")
+			confSlice = append(confSlice, letter)
 		}
 	}
-
+	confSlice = append(confSlice, stop)
 	return confSlice
 }
 
@@ -36,32 +41,30 @@ func Unpack(oldString string) (string, error) {
 	koff := 0
 
 	for index := range sliceString {
-		index = index + koff
+		index += koff
 		if index < len(sliceString) {
 			switch {
-			case confSlice[len(oldString)-1] == "digit":
+			case confSlice[len(oldString)-1] == digit:
 				return "", ErrInvalidString
-			case confSlice[index] == "digit" && confSlice[index+1] == "digit":
+			case confSlice[index] == digit && confSlice[index+1] == digit:
 				return "", ErrInvalidString
-			case confSlice[index] == "digit" && confSlice[index+1] == "slash":
+			case confSlice[index] == digit && confSlice[index+1] == slash:
 				result.WriteString(string(sliceString[index]))
-			case confSlice[index] == "slash":
+			case confSlice[index] == slash:
 				continue
-			case confSlice[index] == "letter" && confSlice[index+1] == "digit":
+			case confSlice[index] == letter && confSlice[index+1] == digit:
 				result.WriteString(string(sliceString[index]))
-			case confSlice[index] == "digit" && confSlice[index+1] == "letter":
+			case confSlice[index] == digit && confSlice[index+1] == letter:
 				count, _ := strconv.Atoi(sliceString[index])
 				result.WriteString(strings.Repeat(string(sliceString[index+1]), count))
-				koff += 1
-			case (confSlice[index] == "letter" || confSlice[index] == "digit") && confSlice[index+1] == "stop":
+				koff++
+			case (confSlice[index] == letter || confSlice[index] == digit) && confSlice[index+1] == stop:
 				result.WriteString(string(sliceString[index]))
 				break
-			case confSlice[index] == "letter" && confSlice[index+1] == "letter":
+			case confSlice[index] == letter && confSlice[index+1] == letter:
 				result.WriteString(string(sliceString[index]))
 			}
-
 		}
-
 	}
 	return stringutil.Reverse(result.String()), nil
 }
